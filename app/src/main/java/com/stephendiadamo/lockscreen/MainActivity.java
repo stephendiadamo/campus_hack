d085ebd7e65f2289a274fcb2ba6f78f41079fe31
Stephen Diadamo <stephen.diadamo@gmail.com>
2017-04-28 23:39:39 +0200

    Added some settings page stuff

--- app/src/main/java/com/stephendiadamo/lockscreen/MainActivity.java
+++ app/src/main/java/com/stephendiadamo/lockscreen/MainActivity.java
@@ -1,15 +1,38 @@
 package com.stephendiadamo.lockscreen;
 
+import android.app.Service;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startService(new Intent(this, LockScreenService.class));
-        finish();
+        setContentView(R.layout.settings);
+
+        TextView startFakeLock = (TextView) findViewById(R.id.settings_start_fake_lock);
+        TextView stopFakeLock = (TextView) findViewById(R.id.settings_stop_fake_lock);
+        final Intent fakeLock = new Intent(this, LockScreenService.class);
+
+        startFakeLock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(fakeLock);
+                Toast.makeText(view.getContext(), "Lockout started", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        stopFakeLock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(fakeLock);
+                Toast.makeText(view.getContext(), "Lockout stopped", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
