89cfa6faa3253541e7dd00f5f944c7053a41affb
antonio <olivian.gilea.ext@pentasys.de>
2017-04-29 15:49:21 +0200

    small edits

--- app/src/main/java/com/stephendiadamo/lockscreen/MainActivity.java
+++ app/src/main/java/com/stephendiadamo/lockscreen/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
         TextView startFakeLock = (TextView) findViewById(R.id.settings_start_fake_lock);
         TextView stopFakeLock = (TextView) findViewById(R.id.settings_stop_fake_lock);
+        TextView fakeDataInput = (TextView) findViewById(R.id.settings_fake_data_input);
         final Intent fakeLock = new Intent(this, LockScreenService.class);
 
         startFakeLock.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 stopService(fakeLock);
                 Toast.makeText(view.getContext(), "Lockout stopped", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        fakeDataInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.fake_data_input);
             }
         });
     }
