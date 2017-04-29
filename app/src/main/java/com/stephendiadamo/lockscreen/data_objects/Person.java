7300e8586baa1230a66f7fb5589bca5626753eb3
Stephen Diadamo <stephen.diadamo@gmail.com>
2017-04-29 16:24:58 +0200

    Added data generator

---
+++ app/src/main/java/com/stephendiadamo/lockscreen/data_objects/Person.java
@@ -0,0 +1,25 @@
+package com.stephendiadamo.lockscreen.data_objects;
+
+/**
+ * Created by stephendiadamo on 2017-04-28.
+ */
+
+public class Person {
+    public String first_name;
+    public String last_name;
+    public String company;
+    public String email;
+    public String phone;
+    public String dateOfBirth;
+    public String address;
+
+    public Person(String first_name, String last_name, String company, String email, String phone, String dateOfBirth, String address) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.company = company;
+        this.email = email;
+        this.phone = phone;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+    }
+}
